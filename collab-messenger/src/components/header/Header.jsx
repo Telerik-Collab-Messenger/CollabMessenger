76a72a352a3720786afe3dc1b8d6c15b59115327
Leharov <viktorleharov@gmail.com>
2024-08-15 12:45:58 +0300

    skeleton

---
+++ collab-messenger/src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import './Header.css';
+
+export default function Header() {
+        
+        return (
+            <div id='header'>
+                <header>
+                    <div id='header-title'>
+                        <p>Collab Messenger</p>
+                    </div>
+                </header>
+            </div>
+        );
+}
---
+++ collab-messenger/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import './Home.css'
+
+export default function Home() {
+    return (
+        <>
+        <div id='home'>
+            <div id='home-title'>
+                <h1>Welcome to Collab Messenger</h1>
+            </div>
+            <div id='home-content'>
+                <p>Collab Messenger is a simple messaging app that allows you to chat with your friends and family.</p>
+                <p>Get started by creating an account or logging in.</p>
+            </div>
+        </div>
+        </>
+    )
+}
