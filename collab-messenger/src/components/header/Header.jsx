40101b3c99cf45d44d6e72256431cfd03a634bb4
Leharov <viktorleharov@gmail.com>
2024-08-16 13:24:46 +0300

    user button

--- collab-messenger/src/components/header/Header.jsx
+++ collab-messenger/src/components/header/Header.jsx
@@ -2,29 +2,42 @@ import './Header.css';
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
 import { AppContext } from '../../state/app.context';
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+
 
 export default function Header() {
     const { user, setAppState } = useContext(AppContext);
 
     const logout = async () => {
         setAppState({ user: null, userData: null });
-        navigate('/login');
+        navigate('/');
       };
 
     const navigate = useNavigate();
         
         return (
-            <div id='header'>
-                <header>
-                    <div id='header-title'>
-                        <p>Collab Messenger</p>
-                        {user && (<>
-                            <span>Welcome,</span>
-                            {/* <button className="button" onClick={detailUser}>{userData.handle}</button> */}
-                            <button className="button" onClick={logout}>Logout</button>
-                        </>)}
-                    </div>
-                </header>
-            </div>
+          <Navbar fixed="top" expand="lg" variant="dark" bg="dark">
+            <Container>
+              <Navbar.Brand id="brand-name">Better Teams</Navbar.Brand>
+              {/* {user && ( */}
+              <DropdownButton
+                    id="dropdown-btn"
+                    variant="success"
+                    title={
+                        <>
+                            <img id="pfp" src="../../../img/defaultUser.jpg"/>
+                            User
+                        </>
+                    }
+                >
+                <Dropdown.Item variant="success" onClick={logout}>Logout</Dropdown.Item>
+              </DropdownButton>{" "}
+              {/*  )} */}
+            </Container>
+          </Navbar>
         );
 }
