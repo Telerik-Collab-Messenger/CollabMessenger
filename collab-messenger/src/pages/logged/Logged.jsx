cbef3360a1e7d6a20cf0cba870e14f34d400946a
Leharov <viktorleharov@gmail.com>
2024-08-18 16:48:23 +0300

    logged css

--- collab-messenger/src/pages/logged/Logged.jsx
+++ collab-messenger/src/pages/logged/Logged.jsx
@@ -1,14 +1,17 @@
-import { AppContext } from "../../state/app.context"
-import { useContext } from 'react';
+import './Logged.css'
+import { Container, Navbar, Button } from "react-bootstrap";
 
 export default function Logged() {
     
-    const { user } = useContext(AppContext);
-    
     return (
-        <div id="core">
-            <h1>CorePage</h1>
-            {/* {console.log(user)} */}
-        </div>
+        <Container id="navbar-container">
+            <Navbar id='navbar' className="d-flex flex-column" expand="lg" variant="dark" bg="light" fixed="center" >
+                <Container id='button-container' style={{ marginTop: 20 }} className="d-flex flex-column">
+                    <Button className='button d-flex justify-content-center align-items-center' variant='success' style={{ borderBottom: '1px solid gray', marginTop: 20 }}>Channels</Button>
+                    <Button className='button d-flex justify-content-center align-items-center' variant='success' style={{ borderBottom: '1px solid gray', marginTop: 20 }}>Chats</Button>
+                    <Button className='button d-flex justify-content-center align-items-center' variant='success' style={{ borderBottom: '1px solid gray', marginTop: 20 }}>Calls</Button>
+                </Container>
+            </Navbar>
+        </Container>
     )
 }
