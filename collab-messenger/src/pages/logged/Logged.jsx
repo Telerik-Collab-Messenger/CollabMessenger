b0b35596401ded34fd92a1f6f37cbf7b53670c63
Leharov <viktorleharov@gmail.com>
2024-09-10 19:22:40 +0300

    display num of active users

--- collab-messenger/src/pages/logged/Logged.jsx
+++ collab-messenger/src/pages/logged/Logged.jsx
@@ -1,12 +1,27 @@
+import { getAllUsersLength } from '../../services/user.services';
 import './Logged.css'
-//import { Container } from "react-bootstrap";
+import { useState, useEffect } from 'react';
 
 export default function Logged() {
-    
+    const [users, setUsers] = useState([]);
+
+    useEffect (() => {
+        getAllUsersLength()
+        .then(usersSnapshot => {
+            if (usersSnapshot.exists()) {
+              const usersData = Object.values(usersSnapshot.val());
+              setUsers(usersData);
+            }
+          })
+          .catch(error => {
+            console.error("Error fetching users:", error);
+          });
+      }, []);
+
     return (
         <div id='main-logged-container'>
             <div id='inner-container'>
-<p>aaaaaaaaaaaaaaaaaaaaaaaaa</p>
+            <h2>Current Active Users: {users.length}</h2>
             </div>
         </div>
     )
